4a81e7e8bade1a83cd34bb4f2bdd756764299651
Sylvain Fertons <sylvain.fertons@infomaniak.ch>
2017-05-16 16:53:39 +0200

    Little fixes

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,22 @@ var coffee = require("gulp-coffee");
 var uglify = require("gulp-uglify");
 var livereload = require("gulp-livereload");
 
-var distPath = "./lib/";
+var libPath = "./lib/";
 var demoPath = "./demo/";
-var libPath = "./src/";
+var srcPath = "./src/";
 var filename = "scroll-appear";
 
 gulp.task("default", function() {
-  gulp.src(libPath + "*.coffee")
+  gulp.src(srcPath + "*.coffee")
     .pipe(coffee())
     .pipe(rename(filename + ".min.js"))
     .pipe(uglify())
     .pipe(gulp.dest(demoPath))
-    .pipe(gulp.dest(distPath))
+    .pipe(gulp.dest(libPath))
     .pipe(livereload());
 });
 
 gulp.task("watch", function() {
   livereload.listen();
-  gulp.watch(libPath + "*.coffee", ["default"]);
+  gulp.watch(srcPath + "*.coffee", ["default"]);
 });
